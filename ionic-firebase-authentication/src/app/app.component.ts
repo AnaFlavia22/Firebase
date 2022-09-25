2432ab2fa5289e1e43769aafef918220ddd2dcdf
Ana Flávia <anaflaviasoaresreis@unifei.edu.br>
2022-09-25 12:59:03 -0300

    Inclusao de Finalizar sessão no menu

--- ionic-firebase-authentication/src/app/app.component.ts
+++ ionic-firebase-authentication/src/app/app.component.ts
@@ -10,6 +10,7 @@ export class AppComponent {
   public appPages = [
     { title: 'Produtos', url: 'cardapio', icon: 'restaurant' },
     { title: 'Comprar', url: 'selecionar', icon: 'cart' },
+    { title: 'Finalizar sessão', url: 'dashboard', icon: 'arrow-forward' },
   ];
   public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
   constructor() {}
